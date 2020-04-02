3e2b4baab481539eb27fd2d19e0a306445b01273
Shaibu Emmanuel <emmanuel.shaibu@andela.com>
2020-04-02 05:29:45 +0100

    Updates

---
+++ precache-manifest.f53c88048717db6f9e68db5f04487f05.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "deda12f70c7c0e94a6e590c93a840836",
+    "url": "/react-sample-validation-debounce/index.html"
+  },
+  {
+    "revision": "a66974c8280777cc4ce0",
+    "url": "/react-sample-validation-debounce/static/css/main.4d221bf8.chunk.css"
+  },
+  {
+    "revision": "ca1cb5d6af4e516ffaa3",
+    "url": "/react-sample-validation-debounce/static/js/2.9de46122.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-sample-validation-debounce/static/js/2.9de46122.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a66974c8280777cc4ce0",
+    "url": "/react-sample-validation-debounce/static/js/main.2ae03fe6.chunk.js"
+  },
+  {
+    "revision": "b0612d4f5626a81aff7f",
+    "url": "/react-sample-validation-debounce/static/js/runtime-main.ff158ae8.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-sample-validation-debounce/precache-manifest.b5b5aed725467c7afa7110ad3b6a3d7b.js"
+  "/react-sample-validation-debounce/precache-manifest.f53c88048717db6f9e68db5f04487f05.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/react-sample-validation-debounce/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-sample-validation-debounce/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
